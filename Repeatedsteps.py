788d9f34b5eeb2f9b8cc99ec6ef255219c389e76
shayar951 <60566074+shayar951@users.noreply.github.com>
2020-02-02 11:46:52 +0200

    Add files via upload

---
+++ Repeatedsteps.py
@@ -0,0 +1,10 @@
+# n = 5 
+# while n > 0 :
+#     print(n)
+#     n = n - 1
+# print('Blastoffl')    
+sh = input('add number')
+while int(sh) > 10:
+    print(sh)
+    int(sh - 1)
+print ('end')    
---
+++ salary.py
@@ -0,0 +1,4 @@
+hours = input('how many hours')
+rate = input('how many rate ')
+pay = int(hours) * int(rate)
+print(pay)
